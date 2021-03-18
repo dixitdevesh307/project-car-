4595ef4394248be11e11fecd83d1425d457d3586
DEVESH DIXIT <79449191+dixitdevesh307@users.noreply.github.com>
2021-03-18 09:41:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var car,wall
+var speed,weight
+function setup() {
+ createCanvas(1600,400)
+  
+  car=createSprite(50,200,50,50);
+  car.shapeColor="white";
+  wall=createSprite(1500,200,60,height/2);
+
+  speed=random(55,90);
+  weight=random(400,1500);
+
+  car.velocityX=speed;
+
+}
+
+function draw() {
+  background("black");  
+
+  if(wall.x-car.x<(car.width+wall.width)/2){
+    car.velocityX=0
+    var  dev=0.5 * speed * weight*  speed/22500 ;
+  
+    if(dev>180)
+   {
+     car.shapeColor="green"
+
+   }
+if(dev<180 && dev>100)
+{ car.shapeColor="red";
+}
+if(dev<100)
+{
+ 
+  car.shapeColor="yellow"
+}
+  
+
+
+}
+  drawSprites();
+
+}
